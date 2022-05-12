0a0573d7da5d5f54a2c3874ec85cecacd89917d6
Julio Guerra <julio@datadog.com>
2022-05-12 17:13:35 +0200

    Add version parsing script

---
+++ _tools/release/parse-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[[ "$1" =~ v([[:digit:]])+\.([[:digit:]])+\.([[:digit:]]+)(-rc\.([[:digit:]])) ]]
+
+echo "::set-output name=version::${BASH_REMATCH[0]}"
+echo "::set-output name=major::${BASH_REMATCH[1]}"
+echo "::set-output name=minor::${BASH_REMATCH[2]}"
+echo "::set-output name=patch::${BASH_REMATCH[3]}"
+echo "::set-output name=rc::${BASH_REMATCH[5]}"
